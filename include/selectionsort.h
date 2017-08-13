a5253c3ac636731ff5e9c708aed8f6983c9ee499
Praveen Abraham <praveen@praveenabraham.com>
2017-08-13 20:39:29 +0100

    Add selection sort

--- include/selectionsort.h
+++ include/selectionsort.h
@@ -2,7 +2,17 @@
 #define SECLECTION_SORT_H_
 
 namespace sorting {
+  template <typename It, typename Pred=std::less<typename std::iterator_traits<It>::value_type>>
+  void SelectionSort(It begin, It end, Pred pred=Pred())
+  {
+    if (std::distance(begin, end) < 1)
+      return;
 
+    for (auto it=begin; it!=end; ++it)
+    {
+      std::swap(*it, *std::min_element(it, end, pred));
+    }
+  }
 }
 
 #endif
