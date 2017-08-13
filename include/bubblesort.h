9cfd0a4d5cebc60610b77bace9c632bb404dc2d3
Praveen Abraham <praveen@praveenabraham.com>
2017-08-13 20:41:37 +0100

    Format bubble sort code

--- include/bubblesort.h
+++ include/bubblesort.h
@@ -2,31 +2,31 @@
 #define BUBBLE_SORT_H_
 
 namespace sorting {
-
   template <typename It, typename Pred=std::less<typename std::iterator_traits<It>::value_type>>
-  void BubbleSort(It begin, It end, Pred pred=Pred()) {
-
+  void BubbleSort(It begin, It end, Pred pred=Pred())
+  {
     if (std::distance(begin, end) < 1)
       return;
 
     bool swap = true;
     auto ite  = end;
 
-    while (swap) {
+    while (swap)
+    {
       swap = false;
       std::advance(ite, -1);
 
-      for (auto it=begin; it!=ite; ++it) {
+      for (auto it=begin; it!=ite; ++it)
+      {
         auto next = std::next(it, 1);
-        if (pred(*next, *it)) {
+        if (pred(*next, *it))
+        {
           std::swap(*it, *next);
           swap = true;
         }
       }
     }
-
   }
-
 }
 
 #endif
